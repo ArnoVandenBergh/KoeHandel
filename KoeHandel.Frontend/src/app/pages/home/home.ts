7051b67a032ed1c2e935faeb724aada1f9c31146
Arno Van den Bergh <arnovdb1234@hotmail.be>
2025-06-13 21:11:27 +0200

    make home page nicer

--- KoeHandel.Frontend/src/app/pages/home/home.ts
+++ KoeHandel.Frontend/src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { Button } from "../../components/button/button";
+import { Title } from "../../components/title/title";
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [Button],
+  imports: [Button, Title],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
